2147d96f5975a9b72462563a45bbba4a8dc24cee
EasyArch-Sun <3268543958@qq.com>
2021-06-02 00:54:10 +0800

    factoryPattern

---
+++ src/factoryPattern/demo/Circle.java
@@ -0,0 +1,8 @@
+package factoryPattern.demo;
+
+public class Circle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Circle:draw() meethod");
+    }
+}
---
+++ src/factoryPattern/demo/Shape.java
@@ -0,0 +1,6 @@
+package factoryPattern.demo;
+
+public interface Shape {
+
+    void draw();
+}
