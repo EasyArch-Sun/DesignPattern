1ad0ca896cd5b25525df4908aa248e90868dcbef
EasyArch-Sun <3268543958@qq.com>
2021-06-18 14:03:32 +0800

    buildPattern

---
+++ src/builderPattern/demo/Pepsi.java
@@ -0,0 +1,13 @@
+package builderPattern.demo;
+
+public class Pepsi extends ColdDrink {
+    @Override
+    public String name() {
+        return "Pepsi";
+    }
+
+    @Override
+    public float price() {
+        return 31.0f;
+    }
+}
