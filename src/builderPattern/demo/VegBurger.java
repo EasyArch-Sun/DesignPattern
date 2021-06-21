a8c1d252039423662b93c0cc616c9c91820be7ee
EasyArch-Sun <3268543958@qq.com>
2021-06-21 18:36:35 +0800

    abstractFactoryPattern

---
+++ src/builderPattern/demo/VegBurger.java
@@ -0,0 +1,15 @@
+package builderPattern.demo;
+
+
+// 5
+public class VegBurger extends Burger {
+    @Override
+    public String name() {
+        return "Veg Burger";
+    }
+
+    @Override
+    public float price() {
+        return 25.0f;
+    }
+}
